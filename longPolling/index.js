e5985e4c1736c309117d886f716dd38786324757
subhamdash45 <mrsubhamdash1998@gmail.com>
2024-03-25 08:31:48 +0530

    feat(long-polling): added long polling

---
+++ longPolling/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const PORT = 3010;
+
+const app = express();
+let data = "Initial Data";
+const waitingClientList = [];
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__filename, "/index.html");
+});
+
+//  use for long polling ---
+app.get("/getData", (req, res) => {
+  if (data !== req.query.lastData) {
+    res.json({ data });
+  } else {
+    waitingClientList.push(res);
+  }
+});
+
+app.get("/updateData", (req, res) => {
+  data = req.query.newData;
+  while(waitingClientList.length>0){
+    const clientRes = waitingClientList.pop()
+    clientRes.json({data})
+  }
+  res.json({})
+});
+
+app.listen(PORT, () => {
+  console.log(`listening to PORT : ${PORT}`);
+});
